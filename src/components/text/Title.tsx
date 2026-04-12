6a087e2c4101759b45891ec5169ca3b22f20756e
pingu204 <73891290+pingu204@users.noreply.github.com>
2026-04-12 19:26:45 +0800

    style: adjust margin in title component

--- src/components/text/Title.tsx
+++ src/components/text/Title.tsx
@@ -8,7 +8,7 @@ const Title = ({ children }: TitleProps) => {
 	return (
 		<>
 			<ScrollAppear>
-				<div className="flex w-full justify-center mb-16">
+				<div className="flex w-full justify-center mb-8">
 					<span className="text-center font-light text-4xl/10 lg:text-6xl/15 xl:text-5xl/13 mb-4 text-center">
 						{children}
 					</span>
